7f123263c3a0ce71f4667d77a93104ba8c29478c
dwidian002 <dwi.dian.s@mail.ukrim.ac.id>
2024-05-27 01:01:52 +0700

    layout-auth-kategori

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('backend.dashboard');
+        return view('backend.content.dashboard');
+    }
+
+    public function profile()
+    {
+        $id = Auth::guard('user')->user() -> id;
+        $user = User::findOrFail($id);
+        return view('backend.content.profile',compact('user'));
+
+    }
+    public function resetPassword()
+    {
+        return view('backend.content.resetPassword');
+    }
+
+    public function prosesResetPassword(Request $request)
+    {
+        $this->validate($request, [
+            'old_password' => 'required',
+            'new_password' => 'required|min:6',
+            'c_new_password' => 'required_with:new_password|same:new_password|min:6'
+        ]);
+
+        $old_password = $request->old_password;
+        $new_password = $request->new_password;
+
+        $id = Auth::guard('user')->user()->id;
+        $user = User::findOrFail($id);
+
+        if (Hash::check($old_password, $user->password)) {
+            $user->password = bcrypt($new_password);
+
+            try {
+                $user->save();
+                return redirect(route('dashboard.resetPassword'))->with('pesan', ['succsess', 'Selamat anda berhasil ubah password']);
+
+            } catch (\Exception $e) {
+                return redirect(route('dashboard.resetPassword'))->with('pesan', ['danger', 'Gagal ubah password']);
+
+            }
+        } else {
+            return redirect(route('dashboard.resetPassword'))->with('pesan', ['danger', 'Password lama salah']);
+
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +20,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', [DashboardController::class,'index'])->name('dashboard.index');
+Route::get('/login', [AuthController::class, 'index'])->name('auth.index')->middleware("guest");
+Route::post('/login', [AuthController::class, 'verify'])->name('auth.verify');
+
+
+Route::group(['middleware' => 'auth:user'], function () {
+    Route::prefix('admin')->group(function () {
+
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+        Route::get('/profile', [DashboardController::class, 'profile'])->name('dashboard.profile');
+        Route::get('/reset-password', [DashboardController::class, 'resetPassword'])->name('dashboard.resetPassword');
+        Route::post('/reset-password', [DashboardController::class, 'prosesResetPassword'])->name('dashboard.prosesResetPassword');
+
+        Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
+        Route::get('/kategori/tambah', [KategoriController::class, 'tambah'])->name('kategori.tambah');
+        Route::post('/kategori/prosesTambah', [KategoriController::class, 'prosesTambah'])->name('kategori.prosesTambah');
+        Route::get('/kategori/ubah/{id}', [KategoriController::class, 'ubah'])->name('kategori.ubah');
+        Route::post('/kategori/prosesUbah', [KategoriController::class, 'prosesUbah'])->name('kategori.prosesUbah');
+        Route::get('/kategori/hapus{id}', [KategoriController::class, 'hapus'])->name('kategori.hapus');
+    });
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
