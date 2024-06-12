30fefca4ed5967aea052ca15c723c347498a38a7
Elexi2242101998 <elexi.violletta.z@mail.ukrim.ac.id>
2024-06-12 19:46:59 +0700

    hal login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>Halleluya Store</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
@@ -20,8 +20,8 @@
 
     <style>
         .bg-gradient-primary {
-            background: linear-gradient(180deg, #ffb9c2 10%, #ffb9c2 100%) !important;
-            background-size: cover;
+            background: url('{{ asset('assets-fe/source/images/aespa.gif') }}') no-repeat center center fixed !important;
+            background-size: cover !important;
         }
 
         .btn-primary {
